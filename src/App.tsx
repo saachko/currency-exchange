92d12aa20b9d7e6c6322229d00da647623932d9e
Anastasiya Sachko <baron-nastya2009@yandex.ru>
2023-04-04 21:06:28 +0300

    feat: add lazy import and loader for fallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { Suspense, lazy, useEffect } from 'react';
 import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
-
-import ConverterPage from 'pages/ConverterPage';
-import CurrencyPage from 'pages/CurrencyPage';
 
 import Footer from 'components/Footer/Footer';
 import Header from 'components/Header/Header';
+import Loader from 'components/Loader/Loader';
+
+const ConverterPage = lazy(() => import('pages/ConverterPage'));
+const CurrencyPage = lazy(() => import('pages/CurrencyPage'));
 
 function App() {
   const location = useLocation();
@@ -21,10 +22,12 @@ function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route index path="converter" element={<ConverterPage />} />
-        <Route path="currency" element={<CurrencyPage />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route index path="converter" element={<ConverterPage />} />
+          <Route path="currency" element={<CurrencyPage />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </>
   );
