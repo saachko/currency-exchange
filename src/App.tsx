e16a44c8c7b0718dee5cde12728de6b4a370fe1b
Anastasiya Sachko <baron-nastya2009@yandex.ru>
2023-04-04 20:33:21 +0300

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import Footer from 'components/Footer/Footer';
 import Header from 'components/Header/Header';
 
 function App() {
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (currentPath === '/') {
+      navigate('/converter');
+    }
+  }, [currentPath]);
+
   return (
     <>
       <Header />
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import clsx from 'clsx';
+import React, { memo } from 'react';
+import { Nav } from 'react-bootstrap';
+import { NavLink, useLocation } from 'react-router-dom';
+
+import Logo from 'components/Logo/Logo';
+
+import { navLinks } from 'utils/constants';
+
+import styles from './Header.module.scss';
 
 function Header() {
-  return <header>Header</header>;
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  return (
+    <header className={styles.header}>
+      <NavLink to="/" className={styles.title}>
+        <Logo />
+        <h1 className="m-0">Конвертер валют</h1>
+      </NavLink>
+      <Nav className={styles.headerNavbar}>
+        {navLinks.map((link) => (
+          <NavLink
+            key={link.id}
+            to={link.path}
+            end
+            className={clsx(styles.headerNavLink, {
+              [styles.active]: currentPath === link.path,
+            })}
+          >
+            {link.name}
+          </NavLink>
+        ))}
+      </Nav>
+    </header>
+  );
 }
 
-export default Header;
+export default memo(Header);
